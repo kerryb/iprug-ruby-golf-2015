175a3b4cfbd00f9be880ae186faa514b3497be7f
Kerry Buckley <kerryjbuckley@gmail.com>
2015-04-14 21:57:45 +0100

    All tests passing – 567 total score

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,28 +1,37 @@
 class Golf
-  def hole_1
+  def hole_1 a
+    ((a>?Z??a:?A)..a).map.with_index{|c,i|c+c*i}.join"\n"
   end
 
-  def hole_2
+  def hole_2 a
+    a.split.sort_by{|w|w.upcase.chars.partition{|x|x !~/AEIOU/}.map(&:length)<<w.upcase}.join" "
   end
 
-  def hole_3
+  def hole_3 a
+    (1..a).select{|x|a%x==0}
   end
 
-  def hole_4
+  def hole_4 x,y
+    ?++?-*x+"+\n"+(?|+" "*x+?|"\n")*y+?++?-*x+?+
   end
 
-  def hole_5
+  def hole_5 a,b
+    a.upcase.chars.grep(/\w/).sort==b.upcase.chars.grep(/\w/).sort
   end
 
-  def hole_6
+  def hole_6 a
+    (["to you"]*3).insert(2,"dear #{a}").map{|x|"Happy birthday #{x}"}.join"\n"
   end
 
-  def hole_7
+  def hole_7 a,n
+    a.gsub(/\W/,"").upcase.bytes.map{|x|(x-65+n)%26+65}.map(&:chr).each_slice(5).map(&:join).join" "
   end
 
-  def hole_8
+  def hole_8 a
+    (?A..?Z).map{|c|"#{c}: #{a.upcase.count c}"}.join"\n"
   end
 
-  def hole_9
+  def hole_9 a
+    a.values.flat_map(&:values).inject{|x,y|x.map{|k,v|[k,v+y[k]]}}.to_h
   end
 end
